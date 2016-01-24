8bb330a2ced474533349d70a44e79603506d76c7
Max Heisinger <mail@maximaximal.com>
2016-01-24 23:44:40 +0100

    Fix an argument error in build_from_jenkins.

--- build_from_jenkins.sh
+++ build_from_jenkins.sh
@@ -6,7 +6,7 @@
 echo "Build BomberPi"
 sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "mkdir -p /root/jenkins/workspace/sfml-sidescroller_x32/build"
 sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "rm -f /root/jenkins/workspace/sfml-sidescroller_x32/build/sfml-sidescroller*"
-sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/sfml-sidescroller_x32/build && cmake .. -DCMAKE_PREFIX_PATH=/usr/local/lib/cmake -DBOMBERPI_VERSION_PATCH="\""$(cat /var/lib/jenkins/jobs/sfml-sidescroller_x32/nextBuildNumber)"\"
+sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/sfml-sidescroller_x32/build && cmake .. -DCMAKE_PREFIX_PATH=/usr/local/lib/cmake -DSFML_SIDESCROLLER_VERSION_PATCH="\""$(cat /var/lib/jenkins/jobs/sfml-sidescroller_x32/nextBuildNumber)"\"
 sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/sfml-sidescroller_x32/build && make"
 sudo chroot /opt/chroots/debian_jessie_x32 /bin/bash -c "cd /root/jenkins/workspace/sfml-sidescroller_x32/build && make package"
 
